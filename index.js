f1eda9a273a2b2c1e5fb62789be57cf2ccb80e4f
Pedro <pkupersmit@gmail.com>
2020-06-07 21:23:46 -0300

    loading bar

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const API_URL = "https://dog.ceo/api/breeds/image/random/3";
 
 const randomDogsElement = document.querySelector(".random-dogs");
 const btn = document.querySelector(".js-button");
+const loadingElement = document.querySelector(".js-loading")
+
+
+
+loadingElement.style.display = "none";
+
+
 
 async function getRandomDogs() {
     randomDogsElement.innerHTML = "";
+    loadingElement.style.display = "";
+
     const response = await fetch(API_URL);
     const json = await response.json();
 
@@ -39,7 +48,11 @@ async function getRandomDogs() {
 
 
     });
+    setTimeout(6000);
+    loadingElement.style.display = "none";
+
 }
+
 
 
 addEventListener("click", getRandomDogs);
